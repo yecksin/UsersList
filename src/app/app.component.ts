90680652140342e2938f8597af24fc65ee2dafc1
yecksin <yecksin@gmail.com>
2020-07-22 22:13:50 -0500

    Base interface for CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,99 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'usersList';
+  title = 'Users List';
+
+  users:User[];
+  userAux:User={
+    firstName:'',
+    Lastname: '',
+    email:'',
+    username:'',
+    userRol:'',
+    userActive:'',
+    editActive: true
+  }
+
+  constructor(){
+    this.getUserts();
+  }
+
+  pushUser(){
+    this.users.push(
+      {
+        firstName:'',
+        Lastname: '',
+        email:'',
+        username:'',
+        userRol:'',
+        userActive:'',
+        editActive: true
+      }
+    );
+    console.log(this.users);
+  }
+  updateUser(index){
+
+  }
+
+  removeUser(){
+
+  }
+
+  getUserts(){
+   this.users=[
+      {
+        firstName:'Yecksin',
+        Lastname: 'Zuñiga',
+        email:'yecksin@gmail.com',
+        username:'yecksin',
+        userRol:'',
+        userActive:'',
+        editActive: false
+      },
+      {
+        firstName:'Sofia',
+        Lastname: 'Cruz',
+        email:'socruz@gmail.com',
+        username:'socruz',
+        userRol:'',
+        userActive:'',
+        editActive: false
+      },
+      {
+        firstName:'Zeus',
+        Lastname: 'Guerrero',
+        email:'zeus@gmail.com',
+        username:'zeus',
+        userRol:'',
+        userActive:'',
+        editActive: false
+      }
+    ]
+  }
+
+  closeAll(thisnot){
+    for (let index = 0; index < this.users.length; index++) {
+      if(index != thisnot )
+      this.users[index].editActive=false;  
+    }
+  }
+
+  sayHello(){
+    console.log("Hello");
+  }
+
+}
+
+
+
+
+export interface User {
+  firstName: string;
+  Lastname: string;
+  email: string;
+  username: string;
+  userRol: string;
+  userActive: string;
+  editActive: boolean;
 }
